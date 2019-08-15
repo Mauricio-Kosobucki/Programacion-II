102ea2c31fecc783943ad768fc17fd3a38ad7252
Julián Álvarez <julianalvarez879@gmail.com>
2019-08-15 11:50:59 -0300

    Clase 3

---
+++ src/TP3/Array_ForEach.java
@@ -0,0 +1,24 @@
+package TP3;
+
+public class Array_ForEach {
+
+    public static void main(String[] args) {
+
+        int[][] Matriz = new int[3][3];
+
+        for (int i=0;   i<3;    i++){
+            for (int j=0;   j<3;    j++){
+                Matriz[i][j] = (int)(Math.random()*100+1);
+            }
+        }
+
+        for (int[] Fila:Matriz){
+            for (int Elemento:Fila){
+                    System.out.print("[" +(Elemento)+ "] ");
+            }
+            System.out.println();
+        }
+
+    }
+
+}
